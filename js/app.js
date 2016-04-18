aceeed2be5f1e666c4f800ce6be05e1fb29c8d2b
Jeffrey Gebhardt <jeff@gebbey.com>
2016-04-18 14:59:59 -0700

    add sales.css

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
+//Pike Place store
+
 var pikePlace = {
   minCustomers: 17,
   maxCustomers: 88,
   avgItemBought: 5.2,
+  total: 0,
   hourlyCustomers: [],
   hourlySales: [],
   hoursOpen: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
@@ -25,14 +28,80 @@ var pikePlace = {
 
   render: function(hourlySales){
     for (var i = 0; i < this.hourlySales.length; i++) {
-      var newLi = document.createElement('li');
+      newLi = document.createElement('li');
       newLi.value = this.hourlySales[i];
-      newLi.textContent = this.hourlySales[i];
+      newLi.textContent = this.hoursOpen[i] + ': ' + this.hourlySales[i] + ' cookies';
       storeOne.appendChild(newLi);
     }
+    for (var i = 0; i < this.hourlySales.length; i++) {
+      this.total += this.hourlySales[i];
+    }
+    console.log(this.total);
+
+    newLi = document.createElement('li');
+    newLi.value = this.total;
+    newLi.textContent = 'Total: ' + this.total + ' cookies';
+    storeOne.appendChild(newLi);
   }
 };
 
 pikePlace.calcHourlyCustomers();
 pikePlace.calcHourlySales();
 pikePlace.render();
+
+//Seatac Airport
+var seatacAirport = {
+  minCustomers: 6,
+  maxCustomers: 18,
+  avgItemBought: 1.2,
+  total: 0,
+  hourlyCustomers: [],
+  hourlySales: [],
+  hoursOpen: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  calcHourlyCustomers: function (minCustomers, maxCustomers){
+
+    for (var i = 0; i < this.hoursOpen.length; i++) {
+      currentHourCustomers = Math.floor(Math.random() * 18) + 6;
+      this.hourlyCustomers.push(currentHourCustomers);
+    }
+    console.log(this.hourlyCustomers);
+  },
+
+  calcHourlySales: function (avgItemBought, hourlyCustomers){
+
+    for (var i = 0; i < this.hourlyCustomers.length; i++) {
+      currentHourSales = Math.floor(this.hourlyCustomers[i] * this.avgItemBought);
+      this.hourlySales.push(currentHourSales);
+    }
+    console.log(this.hourlySales);
+  },
+
+  render: function(hourlySales){
+    for (var i = 0; i < this.hourlySales.length; i++) {
+      newLi = document.createElement('li');
+      newLi.value = this.hourlySales[i];
+      newLi.textContent = this.hoursOpen[i] + ': ' + this.hourlySales[i] + ' cookies';
+      storeTwo.appendChild(newLi);
+    }
+    for (var i = 0; i < this.hourlySales.length; i++) {
+      this.total += this.hourlySales[i];
+    }
+    console.log(this.total);
+
+    newLi = document.createElement('li');
+    newLi.value = this.total;
+    newLi.textContent = 'Total: ' + this.total + ' cookies';
+    storeTwo.appendChild(newLi);
+  }
+};
+
+seatacAirport.calcHourlyCustomers();
+seatacAirport.calcHourlySales();
+seatacAirport.render();
+
+
+//Southcenter
+
+//Bellevue Square
+
+//Alki
